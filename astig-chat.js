e5c0cef192c60a9cb630a99a3e2b09ce22355d2c
majesticwebcreation-ui <majesticwebcreation@gmail.com>
2026-03-03 17:23:58 +1100

    Add files via upload

---
+++ astig-chat.js
@@ -0,0 +1,13 @@
+// Astig Media Chatbot Widget
+// CSS: http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat-widgets.css
+// CDN: http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat.js
+(function() {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat-widgets.css';
+  document.head.appendChild(link);
+  var script = document.createElement('script');
+  script.src = 'http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat.js';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
