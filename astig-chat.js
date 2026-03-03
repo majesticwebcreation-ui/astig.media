579a541b65428a4e680cebe7c229908095663b33
majesticwebcreation-ui <majesticwebcreation@gmail.com>
2026-03-03 17:34:18 +1100

    Update astig-chat.js

--- astig-chat.js
+++ astig-chat.js
@@ -4,10 +4,11 @@
 (function() {
   var link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = 'http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat-widgets.css';
+  link.href = 'https://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat-widgets.css';
   document.head.appendChild(link);
   var script = document.createElement('script');
-  script.src = 'http://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat.js';
+  script.src = 'https://cdn.jsdelivr.net/gh/majesticwebcreation-ui/astig.media/astig-chat.js';
   script.defer = true;
   document.head.appendChild(script);
 })();
+<script src="/path/astig-chat.js?v=20260303-1" defer></script>
