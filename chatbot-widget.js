1cda2d3e65fdc58dc62b8008935a9ee12c3e7f0d
majesticwebcreation-ui <majesticwebcreation@gmail.com>
2026-03-05 17:00:14 +1100

    Deploy chatbot-widget.js from builder (2026-03-05T06:00:12.844Z)

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -21,7 +21,20 @@
     "position": "bottom-right",
     "size": 54
   },
-  "chips": []
+  "chips": [
+    {
+      "id": "chip-1772690401861",
+      "label": "New Action",
+      "icon": "💡",
+      "query": "New Action"
+    },
+    {
+      "id": "chip-1772690403737",
+      "label": "New Action",
+      "icon": "💡",
+      "query": "New Action"
+    }
+  ]
 };
   var PREFIX = "cw3d";
   var STYLE_ID = PREFIX + "-style";
