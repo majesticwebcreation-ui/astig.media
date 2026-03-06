edbb2f6346669c16f7542197a836795485c5c4df
majesticwebcreation-ui <majesticwebcreation@gmail.com>
2026-03-06 15:38:56 +1100

    Deploy astig-chat.js from builder (2026-03-06T04:38:50.408Z)

--- astig-chat.js
+++ astig-chat.js
@@ -1,6 +1,6 @@
 /*__ASTIG_DEPLOY_CONFIG_START__*/
 (function() {
-  var __astigDeployStamp = "2026-03-06T04:31:07.911Z";
+  var __astigDeployStamp = "2026-03-06T04:38:49.691Z";
   var __astigPrevStamp = String(window.__ASTIG_CHAT_DEPLOY_STAMP || '');
   if (__astigPrevStamp && __astigDeployStamp && __astigPrevStamp > __astigDeployStamp) { return; }
   if (__astigDeployStamp) window.__ASTIG_CHAT_DEPLOY_STAMP = __astigDeployStamp;
