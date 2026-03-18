c87430d831f366be755dc99cf4db4f534bc22806
majesticwebcreation-ui <majesticwebcreation@gmail.com>
2026-03-19 08:38:26 +1100

    Deploy anti-vs/fwidgets-chat-embed-runtime.js from builder (2026-03-18T21:38:21.218Z)

--- anti-vs/fwidgets-chat-embed-runtime.js
+++ anti-vs/fwidgets-chat-embed-runtime.js
@@ -1,6 +1,6 @@
 /*__ASTIG_DEPLOY_CONFIG_START__*/
 (function() {
-  var __astigDeployStamp = "2026-03-18T21:35:51.312Z";
+  var __astigDeployStamp = "2026-03-18T21:38:21.054Z";
   var __astigPrevStamp = String(window.__ASTIG_CHAT_DEPLOY_STAMP || '');
   if (__astigPrevStamp && __astigDeployStamp && __astigPrevStamp > __astigDeployStamp) { return; }
   if (__astigDeployStamp) window.__ASTIG_CHAT_DEPLOY_STAMP = __astigDeployStamp;
